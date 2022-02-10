edaf58a597043028535404d639c2620398e49c6c
Jgregory6767 <Jgregory67@aol.com>
2022-02-10 15:05:38 -0500

    Add project files.

---
+++ Variable Exercise Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variable_Exercise_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
